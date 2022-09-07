bb759f5bb4376643373de268b73212703f61b3e1
Houfaf abdessalem <abdeslam.dib.usa@gmail.com>
2022-09-07 22:01:16 +0100

    updates

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -21,7 +21,7 @@ const About = () => {
                 <article className="about__card">
                   <FaAward className="about__icon"/>
                   <h5>Experience</h5>
-                  <small>1+ Years Working Experience</small>
+                  <small>2+ Years Working Experience</small>
                 </article>
                 <article className="about__card">
                   <FiUsers className="about__icon"/>
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -7,7 +7,7 @@ import {IoLogoTwitter} from 'react-icons/io'
 const Footer = () => {
     return (
         <footer>
-          <a href="#" className="footer__logo">Dib</a>
+          <a href="#" className="footer__logo">Dib Coder</a>
           <ul className="permalinks">
             <li><a href="#">Home</a></li>
             <li><a href="#about">About</a></li>
